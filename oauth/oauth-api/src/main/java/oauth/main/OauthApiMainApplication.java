cc902e1704710d36d63f0b9d45e413c64ea39e72
obkwon <obin0125@gmail.com>
2025-01-16 02:08:57 +0900

    메인 모듈 수정

--- oauth/oauth-api/src/main/java/oauth/main/OauthApiMainApplication.java
+++ oauth/oauth-api/src/main/java/oauth/main/OauthApiMainApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OauthMainApplication {
+public class OauthApiMainApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OauthMainApplication.class, args);
+		SpringApplication.run(OauthApiMainApplication.class, args);
 	}
 
 }
