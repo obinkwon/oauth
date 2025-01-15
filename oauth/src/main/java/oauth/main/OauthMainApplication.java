92e8df9c61e24632aed6dff396abcd70d33d7e0d
obkwon <obin0125@gmail.com>
2025-01-15 17:37:21 +0900

    서브모듈 추가

---
+++ oauth/src/main/java/oauth/main/OauthMainApplication.java
@@ -0,0 +1,13 @@
+package oauth.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthMainApplication.class, args);
+	}
+
+}
